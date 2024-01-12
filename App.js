dc95983d007d1c0c1e44804e1e9f3afa80a7eba0
Ashish <apal74180@gmail.com>
2024-01-13 01:23:52 +0530

    2nd episode

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading = React.createElement("h1", {id:'heading'}, "Hello from react");
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from react"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
